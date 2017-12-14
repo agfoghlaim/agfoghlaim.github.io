247dd44bd33c22731e0df80222e4ac37c0c50a09
Agfoghlaim <marieohara.gti@gmail.com>
2017-12-14 17:29:33 +0000

    added contact section(firebase), made cv less unresponsive

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,3 +1,57 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCjrinmvvu1HZ3bn140W1ZRDL4HZwpxL50",
+    authDomain: "agfoghlaim-86aa5.firebaseapp.com",
+    databaseURL: "https://agfoghlaim-86aa5.firebaseio.com",
+    projectId: "agfoghlaim-86aa5",
+    storageBucket: "agfoghlaim-86aa5.appspot.com",
+    messagingSenderId: "886048617658"
+  };
+  firebase.initializeApp(config);
+
+  var messagesRef = firebase.database().ref('messages');
+
+  function saveToFb(theData){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      theData:theData
+    }).then(
+        function(){
+            fbSuccess();
+    }).catch(
+        function(error) {
+            fbFail(error);
+    });
+  }
+
+
+                function fbSuccess() {
+                    // Enable button & show success message
+                    $("#btnSubmit").attr("disabled", false);
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                }
+                function fbFail(error){
+                    console.log("fail ", error);
+                 
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry something went wrong. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+
+                }
+
 $(function() {
 
     $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
@@ -15,46 +69,16 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
+            var theData = {name:name, email:email, phone:phone, message:message};
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+           
+           saveToFb(theData);
+         
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            });
         },
         filter: function() {
             return $(this).is(":visible");

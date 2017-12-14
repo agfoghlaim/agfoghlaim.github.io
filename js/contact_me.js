7ff813d231e68aa5157bd8981ce82aa127cad65a
Agfoghlaim <marieohara.gti@gmail.com>
2017-12-14 17:49:47 +0000

    tidied up

--- js/contact_me.js
+++ js/contact_me.js
@@ -12,6 +12,7 @@
   var messagesRef = firebase.database().ref('messages');
 
   function saveToFb(theData){
+    console.log("hi marie ", theData)
     var newMessageRef = messagesRef.push();
     newMessageRef.set({
       theData:theData
@@ -32,7 +33,7 @@
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your message has been sent, thank you. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -41,7 +42,7 @@
                 }
                 function fbFail(error){
                     console.log("fail ", error);
-                 
+                    
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
